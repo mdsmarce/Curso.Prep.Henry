c103c8f95ebd5a6ebeb01596ecca624624ecfa3e
mdsmarce <mdsmarce@gmail.com>
2022-06-17 12:19:59 -0300

    10-JS-Extra-Enproceso

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var arraynew = [];
+  for(var clave in objeto){
+    arraynew.push([clave,objeto.clave]);
+  } 
+  return arraynew;
 }
 
 
